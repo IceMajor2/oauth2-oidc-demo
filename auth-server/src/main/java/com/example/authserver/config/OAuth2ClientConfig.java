f24b17151b775ef3a9662637503110e76b56ee34
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-07 10:57:28 +0100

    Configure client programmatically with persisting in the database

---
+++ auth-server/src/main/java/com/example/authserver/config/OAuth2ClientConfig.java
@@ -0,0 +1,50 @@
+package com.example.authserver.config;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Configuration
+public class OAuth2ClientConfig {
+
+    @Bean
+    RegisteredClientRepository registeredClientRepository(JdbcTemplate jdbcTemplate) {
+        return new JdbcRegisteredClientRepository(jdbcTemplate);
+    }
+
+    @Bean
+    ApplicationRunner runner(RegisteredClientRepository clientRepository) {
+        return args -> {
+            final String clientId = "myclient";
+            if(clientRepository.findByClientId(clientId) == null) {
+                clientRepository.save(
+                        RegisteredClient
+                                .withId(UUID.randomUUID().toString())
+                                .clientId(clientId)
+                                .clientSecret("{bcrypt}$2a$10$EbXaUVnjrkKoixjefxT5N.PQ2QRSStEv7ZJlP0zIOYu5T8rE0FulO")
+                                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                                .authorizationGrantTypes(grantTypes -> grantTypes.addAll(Set.of(
+                                        AuthorizationGrantType.CLIENT_CREDENTIALS,
+                                        AuthorizationGrantType.AUTHORIZATION_CODE,
+                                        AuthorizationGrantType.REFRESH_TOKEN
+                                )))
+                                .redirectUri("http://127.0.0.1:8082/login/oauth2/code/spring")
+                                .scopes(scopes -> scopes.addAll(Set.of(
+                                        "user.read", "user.write", OidcScopes.OPENID
+                                )))
+                                .build()
+                );
+            }
+        };
+    }
+}
