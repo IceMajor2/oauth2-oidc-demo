f678276fba36df0856f0aef5951aa80f63371517
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-07 09:57:35 +0100

    Create test users

--- auth-server/src/main/java/com/example/authserver/AuthServerApplication.java
+++ auth-server/src/main/java/com/example/authserver/AuthServerApplication.java
@@ -2,6 +2,10 @@ package com.example.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @SpringBootApplication
 public class AuthServerApplication {
@@ -10,4 +14,12 @@ public class AuthServerApplication {
 		SpringApplication.run(AuthServerApplication.class, args);
 	}
 
+	@Bean
+	UserDetailsService userDetailsService() {
+		var userBuilder = User.builder();
+		return new InMemoryUserDetailsManager(
+				userBuilder.roles("USER").username("icem").password("{noop}pass").build(),
+				userBuilder.roles("ADMIN").username("admin").password("{noop}pass").build()
+		);
+	}
 }
